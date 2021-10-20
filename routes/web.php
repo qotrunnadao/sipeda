7cdd90dd9ae6eb7f2162a277515d171c6cd46b07
qotrunnadaoktiriani <qotrunnadaoktiriani@gmail.com>
2021-10-20 19:09:56 +0700

    merapikan route

--- routes/web.php
+++ routes/web.php
@@ -14,10 +14,8 @@ use App\Http\Controllers\KomisiController;
 use App\Http\Controllers\JurusanController;
 use App\Http\Controllers\NilaiTAController;
 use App\Http\Controllers\SeminarController;
-use App\Http\Controllers\SemhasTAController;
 use App\Http\Controllers\StatusTAController;
 use App\Http\Controllers\YudisiumController;
-use App\Http\Controllers\SempropTAController;
 use App\Http\Controllers\PendadaranController;
 use App\Http\Controllers\BerandaAdminController;
 use App\Http\Controllers\KonsultasiTAController;
@@ -49,17 +47,16 @@ Route::get('/error', function () {
     return view('guest.error-page');
 });
 
-//================= DASHBOARD ====================
-
 //=============== ROUTE ADMIN , KOMISI, & DOSEN ====================
 Route::get('/admin/beranda', [BerandaAdminController::class, 'index'])->name('admin.route')->middleware('admin');
 
+//======= MASTER DATA ========
 // Route Tahun Akademik
+Route::get('/admin/tahun-akademik', [TahunAkademikController::class, 'index'])->name('tahunAkademik.index');
 Route::post('/admin/tahun-akademik/store', [TahunAkademikController::class, 'store'])->name('tahunAkademik.store');
 Route::get('/admin/tahun-akademik/edit/{id}', [TahunAkademikController::class, 'edit'])->name('tahunAkademik.edit');
 Route::put('/admin/tahun-akademik/update/{id}', [TahunAkademikController::class, 'update'])->name('tahunAkademik.update');
 Route::get('/admin/tahun-akademik/destroy/{id}', [TahunAkademikController::class, 'destroy'])->name('tahunAkademik.destroy');
-Route::get('/admin/tahun-akademik', [TahunAkademikController::class, 'index']);
 
 // Route Jurusan
 Route::get('/admin/jurusan', [JurusanController::class, 'index']);
@@ -82,14 +79,12 @@ Route::get('/admin/level-user/delete/{id}', [LevelController::class, 'destroy'])
 
 //Route Data User
 Route::get('/admin/data-user', [UserController::class, 'index']);
-
 //Route Data Dosen
 Route::get('/admin/data-dosen', [DosenController::class, 'index']);
-
 //Route Data Komisi
 Route::get('/admin/data-komisi', [KomisiController::class, 'index']);
 
-//Tugas Akhir
+//=========== TUGAS AKHIR ================
 //status TA
 Route::get('/tugas-akhir/statusta', [StatusTAController::class, 'index'])->name('statusta.index');
 Route::post('/tugas-akhir/statusta/store', [StatusTAController::class, 'store'])->name('statusta.store');
@@ -110,9 +105,6 @@ Route::get('/tugas-akhir/data-konsultasi', [KonsultasiTAController::class, 'inde
 Route::get('/tugas-akhir/data-konsultasi/{id}', [KonsultasiTAController::class, 'show'])->name('konsultasi.show');
 Route::get('/tugas-akhir/data-konsultasi/diterima/{konsultasiTA}', [KonsultasiTAController::class, 'diterima'])->name('konsultasi.diterima');
 Route::get('/tugas-akhir/data-pendadaran/ditolak/{konsultasiTA}', [KonsultasiTAController::class, 'ditolak'])->name('konsultasi.ditolak');
-
-//seminar
-Route::get('/tugas-akhir/seminar', [SeminarController::class, 'index']);
 
 //semprop
 Route::get('/tugas-akhir/semprop', [SeminarProposalController::class, 'index'])->name('semprop.index');
@@ -137,6 +129,7 @@ Route::put('/tugas-akhir/spk/download/{filename}', [SPKController::class, 'downl
 Route::put('/tugas-akhir/spk/update/{id}', [SPKController::class, 'update'])->name('spk.update');
 Route::get('/tugas-akhir/spk/destroy/{id}', [SPKController::class, 'destroy'])->name('spk.destroy');
 Route::post('/tugas-akhir/spk/nim/', [SPKController::class, 'nim'])->name('spk.nim');
+
 //nilai TA
 Route::get('/tugas-akhir/nilaita', [NilaiTAController::class, 'index'])->name('nilaita.index');
 Route::post('/tugas-akhir/nilaita/store', [NilaiTAController::class, 'store'])->name('nilaita.store');
@@ -144,9 +137,7 @@ Route::put('/tugas-akhir/nilaita/update/{id}', [NilaiTAController::class, 'updat
 Route::get('/tugas-akhir/nilaita/delete/{id}', [NilaiTAController::class, 'destroy'])->name('nilaita.delete');
 Route::post('/tugas-akhir/nilaita/nim/', [NilaiTAController::class, 'nim'])->name('nilaita.nim');
 
-
-
-//Pendadaran
+//=============== PENDADARAN ===================
 //status pendadaran
 Route::get('pendadaran/status-pendadaran', [StatusPendadaranController::class, 'index'])->name('statuspendadaran.index');
 Route::post('pendadaran/status-pendadaran/store', [StatusPendadaranController::class, 'store'])->name('statuspendadaran.store');
@@ -162,12 +153,14 @@ Route::put('pendadaran/data-pendadaran/update/{id}', [PendadaranController::clas
 Route::get('pendadaran/data-pendadaran/delete/{id}', [PendadaranController::class, 'destroy'])->name('pendadaran.delete');
 Route::get('pendadaran/data-pendadaran/diterima/{pendadaran}', [PendadaranController::class, 'diterima'])->name('pendadaran.diterima');
 Route::get('pendadaran/data-pendadaran/ditolak/{pendadaran}', [PendadaranController::class, 'ditolak'])->name('pendadaran.ditolak');
+
 //nilai pendadaran
 Route::get('pendadaran/nilai-pendadaran', [NilaiPendadaranController::class, 'index'])->name('nilaiPendadaran.index');
 Route::post('pendadaran/nilai-pendadaran/store', [NilaiPendadaranController::class, 'store'])->name('nilaiPendadaran.store');
 Route::put('pendadaran/nilai-pendadaran/update/{id}', [NilaiPendadaranController::class, 'update'])->name('nilaiPendadaran.update');
 Route::get('pendadaran/nilai-pendadaran/delete/{id}', [NilaiPendadaranController::class, 'destroy'])->name('nilaiPendadaran.delete');
 Route::post('/pendadaran/nilai-pendadaran/nim/', [NilaiPendadaranController::class, 'nim'])->name('nilaipendadaran.nim');
+
 // berita acara pendadaran
 Route::get('/pendadaran/beritaacara', [BeritaAcaraPendadaranController::class, 'index'])->name('beritaacarapendadaran.index');
 Route::post('/pendadaran/beritaacara/store', [BeritaAcaraPendadaranController::class, 'store'])->name('beritaacarapendadaran.store');
@@ -177,12 +170,13 @@ Route::get('/pendadaran/beritaacara/destroy/{id}', [BeritaAcaraPendadaranControl
 Route::post('/pendadaran/beritaacara/nim/', [BeritaAcaraPendadaranController::class, 'nim'])->name('beritaacarapendadaran.nim');
 
 
-//=================== yudisium =========================
+//=================== YUDISIUM =========================
 //status yudisium
 Route::get('yudisium/status-yudisium', [StatusYudisiumController::class, 'index'])->name('statusyudisium.index');
 Route::post('yudisium/status-yudisium/store', [StatusYudisiumController::class, 'store'])->name('statusyudisium.store');
 Route::put('yudisium/status-yudisium/update/{id}', [StatusYudisiumController::class, 'update'])->name('statusyudisium.update');
 Route::get('yudisium/status-yudisium/delete/{id}', [StatusYudisiumController::class, 'destroy'])->name('statusyudisium.delete');
+
 //data yudisium
 Route::get('yudisium/data-yudisium', [YudisiumController::class, 'index'])->name('yudisium.index');
 Route::get('yudisium/data-yudisium/create', [YudisiumController::class, 'create'])->name('yudisium.create');
@@ -194,6 +188,7 @@ Route::get('yudisium/data-yudisium/diterima/{yudisium}', [YudisiumController::cl
 Route::get('yudisium/data-yudisium/ditolak/{yudisium}', [YudisiumController::class, 'ditolak'])->name('yudisium.ditolak');
 Route::get('yudisium/data-yudisium/ulang/{yudisium}', [YudisiumController::class, 'ulang'])->name('yudisium.ulang');
 Route::get('yudisium/data-yudisium/selesai/{yudisium}', [YudisiumController::class, 'selesai'])->name('yudisium.selesai');
+
 // sk kelulusan
 Route::get('/yudisium/sk', [SKController::class, 'index'])->name('sk.index');
 Route::post('/yudisium/sk/store', [SKController::class, 'store'])->name('sk.store');
@@ -203,111 +198,21 @@ Route::get('/yudisium/sk/destroy/{id}', [SKController::class, 'destroy'])->name(
 Route::post('/yudisium/sk/nim/', [SKController::class, 'nim'])->name('sk.nim');
 
 
-Route::get('/admin/pendadaran/pengajuan', function () {
-    return view('admin.pendadaran.pengajuan');
-});
-Route::get('/admin/pendadaran/surat-tugas', function () {
-    return view('admin.pendadaran.surattugas');
-});
-Route::get('/admin/pendadaran/nilai', function () {
-    return view('admin.pendadaran.datanilai');
-});
-//yudisium
-Route::get('/admin/yudisium/data-yudisium', function () {
-    return view('admin.yudisium.datayudisium');
-});
-Route::get('/admin/yudisium/undangan', function () {
-    return view('admin.yudisium.undangan');
-});
-Route::get('/admin/yudisium/pengajuan', function () {
-    return view('admin.yudisium.pengajuan');
-});
-Route::get('/admin/yudisium/jadwal', function () {
-    return view('admin.yudisium.jadwal');
-});
-Route::get('/admin/yudisium/upload-sk', function () {
-    return view('admin.yudisium.uploadSK');
-});
-
-
-
 //================= ROUTE KOMISI ====================
 Route::get('/komisi/beranda', function () {
-    return view('komisi.master.beranda')->name('komisi.route');
-});
-Route::get('/komisi/data-mahasiswa', function () {
-    return view('komisi.master.datamahasiswa');
-});
-Route::get('/komisi/data-dosen', function () {
-    return view('komisi.master.datadosen');
-});
-//Tugas Akhir
-Route::get('/komisi/tugas-akhir/data-TA', function () {
-    return view('komisi.TA.dataTA');
-});
-Route::get('/komisi/tugas-akhir/dosen-pembimbing', function () {
-    return view('komisi.TA.dosbing');
-});
-Route::get('/komisi/tugas-akhir/pengajuan', function () {
-    return view('komisi.TA.pengajuan');
-});
-Route::get('/komisi/tugas-akhir/detail-pengajuan', function () {
-    return view('komisi.TA.detailpengajuan');
-});
-Route::get('/komisi/tugas-akhir/nilai', function () {
-    return view('komisi.TA.dosbing');
-});
-//Pendadaran
-Route::get('/komisi/pendadaran/data-pendadaran', function () {
-    return view('komisi.pendadaran.datapendadaran');
-});
-Route::get('/komisi/pendadaran/jadwal-pendadaran', function () {
-    return view('komisi.pendadaran.jadwal');
-});
-Route::get('/komisi/pendadaran/pengajuan', function () {
-    return view('komisi.pendadaran.pengajuan');
-});
-Route::get('/komisi/pendadaran/detail-pengajuan', function () {
-    return view('komisi.pendadaran.detailpengajuan');
-});
-Route::get('/komisi/pendadaran/nilai', function () {
-    return view('komisi.pendadaran.datanilai');
-});
-
-
+    return view('komisi.master.beranda');
+})->name('komisi.route');
 
 //================= ROUTE DOSEN ===========================
 Route::get('/dosen/beranda', function () {
-    return view('dosen.beranda')->name('dosen.route');
-});
-//Tugas Akhir
-Route::get('/dosen/tugas-akhir/data-TA', function () {
-    return view('dosen.TA.datamahasiswa');
-});
-Route::get('/dosen/tugas-akhir/data-konsultasi', function () {
-    return view('dosen.TA.datakonsultasi');
-});
-Route::get('/dosen/tugas-akhir/detail-konsultasi', function () {
-    return view('dosen.TA.detailkonsultasi');
-});
-Route::get('/dosen/tugas-akhir/upload-nilai', function () {
-    return view('dosen.TA.uploadnilai');
-});
-//pendadaran
-Route::get('/dosen/pendadaran/data-pendadaran', function () {
-    return view('dosen.pendadaran.datapendadaran');
-});
-Route::get('/dosen/pendadaran/jadwal', function () {
-    return view('dosen.pendadaran.jadwal');
-});
-Route::get('/dosen/pendadaran/upload-nilai', function () {
-    return view('dosen.pendadaran.uploadnilai');
-});
+    return view('dosen.beranda');
+})->name('dosen.route');
 
 //================= ROUTE MAHASISWA =========================
 Route::get('/mhs/beranda', function () {
     return view('guest.menu');
 })->name('mhs.route')->middleware('mhs');
+
 //Tugas Akhir
 Route::get('/mahasiswa/tugas-akhir/beranda', function () {
     return view('mahasiswa.TA.pages.beranda');
