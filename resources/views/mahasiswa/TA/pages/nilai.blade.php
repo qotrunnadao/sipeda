233bda1943fdbd212cf9e84329b68421b08de577
qotrunnadaoktiriani <qotrunnadaoktiriani@gmail.com>
2021-11-13 13:16:30 +0700

    nada

--- resources/views/mahasiswa/TA/pages/nilai.blade.php
+++ resources/views/mahasiswa/TA/pages/nilai.blade.php
@@ -7,7 +7,7 @@
         <div class="card">
             <div class="card-body">
                 <div class="table-responsive">
-                    <table class="table table-striped">
+                    <table class="table table-striped" id="datatable">
                         <thead>
                             <tr class="text-center">
                                 <th> # </th>
