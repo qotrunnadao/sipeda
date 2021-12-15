cdb0a934a7efa12b66b25d276299f08a447ca613
Qotrunnada Oktiriani <qotrunnada.oktiriani@mhs.unsoed.ac.id>
2021-12-15 21:01:38 +0700

    atun

--- resources/views/mahasiswa/yudisium/pages/pendaftaran.blade.php
+++ resources/views/mahasiswa/yudisium/pages/pendaftaran.blade.php
@@ -3,73 +3,91 @@
 @section('icon', 'file')
 @section('content')
 <div class="row">
-    <div class="col-12 grid-margin stretch-card">
+    <div class="col-6 grid-margin stretch-card">
         <div class="card card-primary">
-            <form>
+            <form action="" method="post" enctype="multipart/form-data" id="creatData">
+                @csrf
+                <input type="hidden" class="form-control" id="thnAkad_id" name="thnAkad_id" value="">
+                <input type="hidden" class="form-control" id="mahasiswa_id" name="mahasiswa_id" value="">
                 <div class="card-body">
-                    <div class="form-group row">
-                        <label class="col-sm-2 col-form-label">
-                            Nama
+                    <div class="form-group">
+                        <label>
+                            Berkas Persyaratan
                         </label>
-                        <div class="col-sm-10">
-                            <input type="text" class="form-control" required autofocus />
+                        <div class="input-group">
+                            <input type="file" class="form-control" name="berkas" id="berkas" />
                         </div>
                     </div>
-                    <div class="form-group row">
-                        <label class="col-sm-2 col-form-label">
-                            Transkip Nilai
-                        </label>
-                        <div class="col-sm-10">
-                            <input type="file" class="form-control" />
-                        </div>
-                    </div>
-                    <div class="form-group row">
-                        <label class="col-sm-2 col-form-label">
-                            Hasil Ujian UEPT
-                        </label>
-                        <div class="col-sm-10">
-                            <input type="file" class="form-control" />
-                        </div>
-                    </div>
-                    <button type="button" class="btn btn-gradient-primary"><i class="mdi mdi-content-save"></i> Simpan</button>
+                    <button type="submit" id="btnSubmit" class="btn btn-gradient-primary"><i class="mdi mdi-content-save"></i> Simpan</button>
                 </div>
             </form>
         </div>
     </div>
-
+    <div class="col-md-6 stretch-card grid-margin">
+        <div class="card bg-primary card-img-holder text-white  card-hover">
+            <div class="card-body">
+                <h4 class="font-weight-normal mb-3">Berkas Persyaratan Meliputi
+                </h4>
+                <ul>
+                    <li>Bukti Validasi Nilai Transkrip Akademik Dengan Ketua Program Studi</li>
+                    <li>Judul Tugas Akhir dalam bahasa Indonesia dan Inggris di Acc oleh
+                        pembimbing I dan II</li>
+                    <li>Foto Copy Ijasah SMA/SMK</li>
+                    <li>Nilai UEPT > 400</li>
+                    <li>Lembar pengesahan Tugas Akhir asli ( Cap basah)</li>
+                    <li>Bukti penyerahan laporan Tugas Akhir ke Perpus Unsoed
+                    </li>
+                    <li>Form Distribusi Soft Dan Hard Laporan Ta Ke Perpus Fakultas Teknik</li>
+                    <li>Surat Bebas Perpus Fakultas Teknik</li>
+                    <li>Surat Bebas Perpus Unsoed</li>
+                    <li>Surat Bebas Lab Dari Teknik Elektro/Sipil/ Geologi/ Infomtk Dan Industri</li>
+                    <li>Sertifikat PKKMB</li>
+                    <li>Surat Keterangan Pendamping Ijasah (SKPI) Dan Foto Copy Data Dukung
+                        SKPI (Sertifikat, Piagam, Dll Maximal 3 Terbaik)</li>
+                    <li>Unggah Artikel Ilmiah Di SIA Unsoed ditandatangani mahasiswa Dan
+                        Pembimbing Tugas Akhir</li>
+                    <li>SPK (surat perintah Tugas Akhir) dan kartu kendali Tugas Akhir</li>
+                    <li>Pas Foto hitam putih 3x4 = 2 lembar (foto diberi nama dan nim)</li>
+                    <span class="badge badge-danger">Berkas dijadikan 1 file PDF</span>
+                </ul>
+                <hr class="border-light">
+            </div>
+        </div>
+    </div>
+</div>
+<div class="row">
     <div class="col-12 grid-margin stretch-card">
         <div class="card">
             <div class="card-body">
-                <h4 class="card-title mb-4">Data Pendaftaran</h4>
+                <h4 class="card-title mb-4">Data Pengajuan</h4>
                 <div class="table-responsive mt-3">
                     <table class="table table-striped">
                         <tbody>
                             <tr>
                                 <td> Nama</td>
                                 <td>:</td>
-                                <td> Qotrunnada Oktiriani </td>
+                                <td> </td>
                             </tr>
                             <tr>
                                 <td> Diajukan Pada </td>
                                 <td>:</td>
-                                <td> 20/02/2022 </td>
+                                <td> </td>
                             </tr>
                             <tr>
-                                <td> Transkip Nilai </td>
+                                <td> Berkas </td>
                                 <td>:</td>
-                                <td> Transkip Nilai - H1D018033 </td>
+                                <td> </td>
+                            </tr>
+                            <td> Status Pengajuan </td>
+                            <td>:</td>
+                            <td>
+
+                            </td>
                             </tr>
                             <tr>
-                                <td> Hasil Ujian UEPT </td>
-                                <td>:</td>
-                                <td> Nilai UEPT - H1D018033 </td>
-                            </tr>
-                            <tr>
-                                <td> Status Pendaftaran </td>
-                                <td>:</td>
-                                <td>
-                                    <div class="badge badge-success badge-pill">Disetujui</div>
-                                </td>
+                                <td> Keterangan </td>
+                                <td> : </td>
+                                <td> - </td>
                             </tr>
                         </tbody>
                     </table>
--- resources/views/yudisium/dataYudisium/index.blade.php
+++ resources/views/yudisium/dataYudisium/index.blade.php
@@ -17,9 +17,9 @@
                                 <th> Nama Mahasiswa </th>
                                 <th> NIM</th>
                                 <th> Jurusan</th>
-                                <th> Transkip Nilai </th>
+                                <th> Berkas Persyaratan </th>
                                 <th> status </th>
-                                <th> Verifikasi </th>
+                                {{-- <th> Verifikasi </th> --}}
                                 <th> Aksi </th>
                             </tr>
                         </thead>
@@ -46,7 +46,7 @@
                                     <span class="badge badge-primary">Selesai</span>
                                     @endif
                                 </td>
-                                <td>
+                                {{-- <td>
                                     <div class="btn-group">
                                         <a href="{{ route('yudisium.diterima', $value->id) }}" class="btn btn-gradient-success btn-sm"><i class="mdi mdi-check"></i></a>
                                     </div>
@@ -56,7 +56,7 @@
                                     <div class="btn-group">
                                         <a href="{{ route('yudisium.ditolak', $value->id) }}" class="btn btn-gradient-danger btn-sm "><i class="mdi mdi-close"></i></a>
                                     </div>
-                                </td>
+                                </td> --}}
                                 <td>
                                     <div class="btn-group">
                                         <a href="{{ route('yudisium.edit', $value->id) }}" class="btn btn-gradient-primary btn-sm"><i class="mdi mdi-border-color"></i></a>
@@ -77,71 +77,5 @@
             </div>
         </div>
     </div>
-
-    {{-- <div class="col-12 grid-margin stretch-card">
-        <div class="card">
-            @foreach ($yudisium as $value )
-            <div class="card-body">
-                <div>
-                    <a href="{{ route('yudisium.create') }}" type="button" class="btn btn-sm btn-gradient-primary float-right"> <i class="mdi mdi-plus"></i> Tambah</a>
-                </div>
-                <h4 class="card-title mb-4">Data Pendaftaran</h4>
-                <div class="table-responsive mt-3">
-                    <table class="table table-striped">
-                        <tbody>
-                            <tr>
-                                <td> Nama</td>
-                                <td>:</td>
-                                <td> {{ $value->mahasiswa->nama }} </td>
-                            </tr>
-                            <tr>
-                                <td> NIM</td>
-                                <td>:</td>
-                                <td> {{ $value->mahasiswa->nim }} </td>
-                            </tr>
-                            <tr>
-                                <td> Jurusan</td>
-                                <td>:</td>
-                                <td> {{ $value->mahasiswa->jurusan->namaJurusan }} </td>
-                            </tr>
-                            <tr>
-                                <td> Diajukan Pada </td>
-                                <td>:</td>
-                                <td> {{ $value->created_at }} </td>
-                            </tr>
-                            <tr>
-                                <td> Transkip Nilai </td>
-                                <td>:</td>
-                                <td> {{ $value->transkip }} </td>
-                            </tr>
-                            <tr>
-                                <td> Status Yudisium </td>
-                                <td>:</td>
-                                <td>
-                                    @if($value->status_id == 1)
-                                    <span class="badge badge-warning">Review Bapendik</span>
-                                    @elseif($value->status_id == 2)
-                                    <span class="badge badge-success">Disetujui</span>
-                                    @elseif($value->status_id == 3)
-                                    <span class="badge badge-danger">Tidak disetujui</span>
-                                    @elseif($value->status_id == 4)
-                                    <span class="badge badge-primary">boleh ajukan lagi</span>
-                                    @else
-                                    <span class="badge badge-primary">Selesai</span>
-                                    @endif
-                                </td>
-                            </tr>
-                            <tr>
-                                <td> Keterangan</td>
-                                <td>:</td>
-                                <td> {{ $value->ket }} </td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-            </div>
-            @endforeach
-        </div>
-    </div> --}}
 </div>
 @endsection
