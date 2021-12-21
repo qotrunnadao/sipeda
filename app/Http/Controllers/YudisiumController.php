d920d2900a14aa67ea4a91d21f15b5bfb20b3a80
Qotrunnada Oktiriani <qotrunnada.oktiriani@mhs.unsoed.ac.id>
2021-12-21 16:02:15 +0700

    atun

--- app/Http/Controllers/YudisiumController.php
+++ app/Http/Controllers/YudisiumController.php
@@ -18,225 +18,203 @@ use RealRashid\SweetAlert\Facades\Alert;
 
 class YudisiumController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        $status = StatusYudisium::get()->all();
-        $id = auth()->user()->id;
-        $user_id = User::with(['dosen'])->where('id', $id)->get()->first();
-        $dosen_id = Dosen::with(['user'])->where('user_id', $id)->get()->first();
-        $periode = PeriodeYudisium::where('aktif', '1')->get()->all();
-        $yudisium = Yudisium::with(['periodeyudisium'])->latest()->get();
-        // dd($yudisium);
-        if (auth()->user()->level_id == 2) {
-            $yudisium = Yudisium::with(['mahasiswa', 'periodeYudisium', 'statusYudisium'])->latest()->get();
-            $acc_yudisium = Yudisium::with(['mahasiswa', 'periodeYudisium', 'statusYudisium'])->where('status_id', 2)->latest()->get();
-        } elseif (auth()->user()->level_id == 5) {
-            $yudisium = Yudisium::with(['statusYudisium'])->whereHas('mahasiswa', function ($q) use ($dosen_id) {
-                $q->where('jurusan_id', $dosen_id->jurusan_id);
-            })->latest()->get();
-            $acc_yudisium = Yudisium::with('statusYudisium')->where('status_id', 4)->whereHas('mahasiswa', function ($q) use ($dosen_id) {
-                $q->where('jurusan_id', $dosen_id->jurusan_id);
-            })->latest()->get();
-        }
-        return view('yudisium.dataYudisium.index', compact('yudisium', 'status', 'periode', 'acc_yudisium'));
-    }
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function index()
+	{
+		$status = StatusYudisium::get()->all();
+		$id = auth()->user()->id;
+		$user_id = User::with(['dosen'])->where('id', $id)->get()->first();
+		$dosen_id = Dosen::with(['user'])->where('user_id', $id)->get()->first();
+		$periode = PeriodeYudisium::where('aktif', '1')->get()->all();
+		$yudisium = Yudisium::with(['periodeyudisium'])->latest()->get();
+		// dd($yudisium);
+		if (auth()->user()->level_id == 2) {
+			$yudisium = Yudisium::with(['mahasiswa', 'periodeYudisium', 'statusYudisium'])->latest()->get();
+			$acc_yudisium = Yudisium::with(['mahasiswa', 'periodeYudisium', 'statusYudisium'])->where('status_id', 2)->latest()->get();
+		} elseif (auth()->user()->level_id == 5) {
+			$yudisium = Yudisium::with(['statusYudisium'])->whereHas('mahasiswa', function ($q) use ($dosen_id) {
+				$q->where('jurusan_id', $dosen_id->jurusan_id);
+			})->latest()->get();
+			$acc_yudisium = Yudisium::with('statusYudisium')->where('status_id', 4)->whereHas('mahasiswa', function ($q) use ($dosen_id) {
+				$q->where('jurusan_id', $dosen_id->jurusan_id);
+			})->latest()->get();
+		}
+		return view('yudisium.dataYudisium.index', compact('yudisium', 'status', 'periode', 'acc_yudisium'));
+	}
 
-    public function laporan()
-    {
-        $periode = PeriodeYudisium::where('aktif', '1')->get()->all();
-        // dd($yudisium);
-        return view('yudisium.dataYudisium.cetaksk', compact('periode'));
-    }
-    public function cetaksk()
-    {
-        $periode = PeriodeYudisium::where('aktif', '1')->get()->all();
-        $pdf = PDF::loadView('yudisium.periode.berkas', ['periode' => $periode])->setPaper('a4', 'landscape');
+	public function laporan()
+	{
+		$periode = PeriodeYudisium::where('aktif', '1')->get()->all();
+		// dd($yudisium);
+		return view('yudisium.dataYudisium.cetaksk', compact('periode'));
+	}
+	public function cetaksk()
+	{
+		$periode = PeriodeYudisium::where('aktif', '1')->get()->all();
+		$pdf = PDF::loadView('yudisium.periode.berkas', ['periode' => $periode])->setPaper('a4', 'landscape');
 
-        $m = new Merger();
+		// dd($yudisium);
+	}
+	/**
+	 * Show the form for creating a new resource.
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function create()
+	{
+		$action = route('yudisium.store');
+		$button = 'Tambah';
+		$data_yudisium = new Yudisium();
+		$yudisium = Yudisium::get();
+		$periode = PeriodeYudisium::where('aktif', '1')->get()->all();
+		$status = StatusYudisium::get();
+		$tahun = TahunAkademik::where('aktif', '1')->get()->first();
+		// dd($tahun);
+		$jurusan = jurusan::get();
+		return view('yudisium.dataYudisium.form', compact('action', 'button', 'data_yudisium', 'status', 'tahun', 'jurusan', 'periode'));
+	}
 
-        $view1 = \View::make('pdf.informe1_vertical')->render();
-        $view2 = \View::make('pdf.informe2_horitzontal')->render();
-        $view3 = \View::make('pdf.informe3_vertical')->render();
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @return \Illuminate\Http\Response
+	 */
+	public function nim(Request $request)
+	{
+		$mahasiswa = Mahasiswa::Has('TA')->whereHas('TA', function ($q) use ($request) {
+			$q->where('status_id', '10');
+		})->Has('Pendadaran')->whereHas('Pendadaran', function ($q) {
+			$q->where('statuspendadaran_id', '6');
+		})->whereDoesntHave('Yudisium')->orHas('Yudisium')->whereHas('Yudisium', function ($q) {
+			$q->where('status_id', '1');
+		})->where('jurusan_id', $request->id)->get();
+		// dd($mahasiswa);
+		return response()->json($mahasiswa, 200);
+	}
+	public function store(Request $request)
+	{
+		$this->validate($request, [
+			"berkas" => "mimes:pdf|max:10000"
+		]);
+		$data = $request->all();
+		$mhs_id = Mahasiswa::where('id', $request->nim)->get()->first();
+		$nim = $mhs_id->nim;
+		if ($request->file('berkas')) {
+			$file = $request->file('berkas');
+			$filename = 'Yudisium' . '_' . $nim . '_' . time() . '.' . $file->getClientOriginalExtension();
+			$path = $request->file('berkas')->storeAS('public/assets/file/Yudisium/', $filename);
+			// dd($filename);
+			$data = [
+				'mhs_id' => $request->nim,
+				'thnAkad_id' => $request->thnAkad_id,
+				'status_id' => $request->statusyudisium_id,
+				'berkas' => $filename,
+				'periode_id' => $request->periode_id,
+				'ket' => $request->ket,
+			];
+			// dd($data);
+			$cek = Yudisium::create($data);
+		}
 
-        $pdf = App::make('dompdf.wrapper');
-        $pdf->loadHTML($view1)->setPaper('a4', 'portrait');
-        $m->addRaw($pdf->output());
+		if ($cek == true) {
+			Alert::success('Berhasil', 'Berhasil Tambah Data Pengajuan Yudisium');
+		} else {
+			Alert::warning('Gagal', 'Data Pengajuan Yudisium Gagal Ditambahkan');
+		}
 
-        $pdf = App::make('dompdf.wrapper');
-        $pdf->loadHTML($view2)->setPaper('a4', 'landscape');
-        $m->addRaw($pdf->output());
+		return redirect(route('yudisium.index'));
+	}
 
-        $pdf = App::make('dompdf.wrapper');
-        $pdf->loadHTML($view3)->setPaper('a4', 'portrait');
-        $m->addRaw($pdf->output());
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param  \App\Models\Yudisium  $yudisium
+	 * @return \Illuminate\Http\Response
+	 */
+	public function show(Yudisium $yudisium)
+	{
+		//
+	}
 
-        file_put_contents('informes/test_' . $user->id . '.pdf', $m->merge());
+	/**
+	 * Show the form for editing the specified resource.
+	 *
+	 * @param  \App\Models\Yudisium  $yudisium
+	 * @return \Illuminate\Http\Response
+	 */
+	public function edit($id)
+	{
+		$yudisium = Yudisium::get();
+		$data_yudisium = Yudisium::find($id);
+		$button = 'Edit';
+		$action = url('/yudisium/data-yudisium/update');
+		$status = StatusYudisium::get();
+		return view('yudisium.dataYudisium.form', compact('action', 'button', 'data_yudisium', 'status'));
+	}
 
-        return $pdf->stream();
+	/**
+	 * Update the specified resource in storage.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @param  \App\Models\Yudisium  $yudisium
+	 * @return \Illuminate\Http\Response
+	 */
+	public function update(Request $request, $id)
+	{
+		$yudisium = Yudisium::find($id);
+		$data = $request->all();
+		// dd($data);
+		$yudisium->update($data);
+		Alert::success('Berhasil', 'Berhasil edit data Yudisium');
+		return back();
+	}
 
-        // dd($yudisium);
-    }
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        $action = route('yudisium.store');
-        $button = 'Tambah';
-        $data_yudisium = new Yudisium();
-        $yudisium = Yudisium::get();
-        $periode = PeriodeYudisium::where('aktif', '1')->get()->all();
-        $status = StatusYudisium::get();
-        $tahun = TahunAkademik::where('aktif', '1')->get()->first();
-        // dd($tahun);
-        $jurusan = jurusan::get();
-        return view('yudisium.dataYudisium.form', compact('action', 'button', 'data_yudisium', 'status', 'tahun', 'jurusan', 'periode'));
-    }
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @param  \App\Models\Yudisium  $yudisium
+	 * @return \Illuminate\Http\Response
+	 */
+	public function destroy($id)
+	{
+		$yudisium = Yudisium::find($id);
+		$yudisium->delete();
+		File::delete(public_path('storage/assets/file/Yudisium/' . $yudisium->berkas . ''));
+		Alert::success('Berhasil', 'Berhasil hapus data Yudisium');
+		return back();
+	}
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function nim(Request $request)
-    {
-        $mahasiswa = Mahasiswa::Has('TA')->whereHas('TA', function ($q) use ($request) {
-            $q->where('status_id', '10');
-        })->Has('Pendadaran')->whereHas('Pendadaran', function ($q) {
-            $q->where('statuspendadaran_id', '6');
-        })->whereDoesntHave('Yudisium')->orHas('Yudisium')->whereHas('Yudisium', function ($q) {
-            $q->where('status_id', '1');
-        })->where('jurusan_id', $request->id)->get();
-        // dd($mahasiswa);
-        return response()->json($mahasiswa, 200);
-    }
-    public function store(Request $request)
-    {
-        $this->validate($request, [
-            "berkas" => "mimes:pdf|max:10000"
-        ]);
-        $data = $request->all();
-        $mhs_id = Mahasiswa::where('id', $request->nim)->get()->first();
-        $nim = $mhs_id->nim;
-        if ($request->file('berkas')) {
-            $file = $request->file('berkas');
-            $filename = 'Yudisium' . '_' . $nim . '_' . time() . '.' . $file->getClientOriginalExtension();
-            $path = $request->file('berkas')->storeAS('public/assets/file/Yudisium/', $filename);
-            // dd($filename);
-            $data = [
-                'mhs_id' => $request->nim,
-                'thnAkad_id' => $request->thnAkad_id,
-                'status_id' => $request->statusyudisium_id,
-                'berkas' => $filename,
-                'periode_id' => $request->periode_id,
-                'ket' => $request->ket,
-            ];
-            // dd($data);
-            $cek = Yudisium::create($data);
-        }
-
-        if ($cek == true) {
-            Alert::success('Berhasil', 'Berhasil Tambah Data Pengajuan Yudisium');
-        } else {
-            Alert::warning('Gagal', 'Data Pengajuan Yudisium Gagal Ditambahkan');
-        }
-
-        return redirect(route('yudisium.index'));
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Yudisium  $yudisium
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Yudisium $yudisium)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Yudisium  $yudisium
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        $yudisium = Yudisium::get();
-        $data_yudisium = Yudisium::find($id);
-        $button = 'Edit';
-        $action = url('/yudisium/data-yudisium/update');
-        $status = StatusYudisium::get();
-        return view('yudisium.dataYudisium.form', compact('action', 'button', 'data_yudisium', 'status'));
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Yudisium  $yudisium
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        $yudisium = Yudisium::find($id);
-        $data = $request->all();
-        // dd($data);
-        $yudisium->update($data);
-        Alert::success('Berhasil', 'Berhasil edit data Yudisium');
-        return back();
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Yudisium  $yudisium
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        $yudisium = Yudisium::find($id);
-        $yudisium->delete();
-        File::delete(public_path('storage/assets/file/Yudisium/' . $yudisium->berkas . ''));
-        Alert::success('Berhasil', 'Berhasil hapus data Yudisium');
-        return back();
-    }
-
-    public function diterima(Yudisium $yudisium)
-    {
-        $data = array(
-            'status_id' => 3,
-        );
-        $yudisium->update($data);
-        Alert::success('Berhasil', 'Pengajuan Yudisium Diterima');
-        return back();
-    }
-    public function ditolak(Yudisium $yudisium)
-    {
-        $data = array(
-            'status_id' => 1,
-        );
-        $yudisium->update($data);
-        Alert::warning('Berhasil', 'Pengajuan Yudisium Ditolak');
-        return back();
-    }
-    public function download($filename)
-    {
-        //    dd($filename);
-        if (File::exists(public_path('storage/assets/file/Yudisium/' . $filename . ''))) {
-            return response()->File(public_path('storage/assets/file/Yudisium/' . $filename . ''));
-        } else {
-            Alert::warning('Gagal', 'File Tidak Tersedia');
-            return back();
-        }
-    }
+	public function diterima(Yudisium $yudisium)
+	{
+		$data = array(
+			'status_id' => 3,
+		);
+		$yudisium->update($data);
+		Alert::success('Berhasil', 'Pengajuan Yudisium Diterima');
+		return back();
+	}
+	public function ditolak(Yudisium $yudisium)
+	{
+		$data = array(
+			'status_id' => 1,
+		);
+		$yudisium->update($data);
+		Alert::warning('Berhasil', 'Pengajuan Yudisium Ditolak');
+		return back();
+	}
+	public function download($filename)
+	{
+		//    dd($filename);
+		if (File::exists(public_path('storage/assets/file/Yudisium/' . $filename . ''))) {
+			return response()->File(public_path('storage/assets/file/Yudisium/' . $filename . ''));
+		} else {
+			Alert::warning('Gagal', 'File Tidak Tersedia');
+			return back();
+		}
+	}
 }
