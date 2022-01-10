04f89c7e3f7485050d5bf2bdb54c983fdb54336b
Himawan Zidan Prayoga <himawanhidan@gmail.com>
2022-01-10 19:58:10 +0700

    Doyok

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -47,7 +47,7 @@ class LoginController extends Controller
     public function login(Request $request)
     {
         $request->validate([
-            'email' => 'required|email:dns',
+            'email' => 'required',
             'password' => 'required',
         ]);
 
